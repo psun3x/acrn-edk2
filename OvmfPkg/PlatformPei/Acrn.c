psun3x/acrn-edk2

commit e7c3a003539855078fd49b93260533fbf65b5408

Use ACRN E820 map to query system memory size

OvmfPkg/PlatformPei/Acrn.c
@@ -46,6 +46,70 @@ E820IsValid (
 
 
 RETURN_STATUS
+AcrnGetSystemMemorySizeBelow4gb (
+  OUT  UINT32               *MemSize OPTIONAL
+  )
+{
+  ACRN_E820_INFO    *E820;
+  INT32             Loop;
+  EFI_E820_ENTRY64  *Entry;
+
+  //
+  // Parse E820 map
+  //
+  E820 = (VOID *)ACRN_E820_PHYSICAL_ADDRESS;
+
+  if (!E820IsValid (E820)) {
+    return RETURN_UNSUPPORTED;
+  }
+
+  for (Loop = (INT32)(E820->E820EntriesCount - 1), Entry = &E820->E820Map[Loop];
+       Loop >= 0; Entry = &E820->E820Map[--Loop]) {
+    if (Entry->BaseAddr + Entry->Length < BASE_4GB &&
+        Entry->Type == EfiAcpiAddressRangeMemory) {
+      *MemSize = Entry->BaseAddr + Entry->Length;
+      return RETURN_SUCCESS;
+    }
+  }
+
+  return RETURN_NOT_FOUND;
+}
+
+
+RETURN_STATUS
+AcrnGetSystemMemorySizeAbove4gb (
+  OUT  UINT64               *MemSize OPTIONAL
+  )
+{
+  ACRN_E820_INFO    *E820;
+  INT32             Loop;
+  EFI_E820_ENTRY64  *Entry;
+  UINT64            TotalMemSize = 0;
+
+  //
+  // Parse E820 map
+  //
+  E820 = (VOID *)ACRN_E820_PHYSICAL_ADDRESS;
+
+  if (!E820IsValid (E820)) {
+    return RETURN_UNSUPPORTED;
+  }
+
+  for (Loop = (INT32)(E820->E820EntriesCount - 1), Entry = &E820->E820Map[Loop];
+       Loop >= 0 && Entry->BaseAddr + Entry->Length > BASE_4GB;
+       Entry = &E820->E820Map[--Loop]) {
+    ASSERT (Entry->BaseAddr >= BASE_4GB);
+    if (Entry->Type == EfiAcpiAddressRangeMemory) {
+      TotalMemSize += Entry->Length;
+    }
+  }
+
+  *MemSize = TotalMemSize;
+  return RETURN_SUCCESS;
+}
+
+
+RETURN_STATUS
 AcrnGetFirstNonAddress (
   OUT  UINT64               *MaxAddress OPTIONAL
   )

OvmfPkg/PlatformPei/MemDetect.c
@@ -212,8 +212,13 @@ GetSystemMemorySizeBelow4gb (
   VOID
   )
 {
-  UINT8 Cmos0x34;
-  UINT8 Cmos0x35;
+  UINT32 AcrnMemSizeBelow4gb;
+  UINT8  Cmos0x34;
+  UINT8  Cmos0x35;
+
+  if (AcrnGetSystemMemorySizeBelow4gb (&AcrnMemSizeBelow4gb) == RETURN_SUCCESS) {
+    return AcrnMemSizeBelow4gb;
+  }
 
   //
   // CMOS 0x34/0x35 specifies the system memory above 16 MB.
@@ -236,8 +241,13 @@ UINT64
 GetSystemMemorySizeAbove4gb (
   )
 {
+  UINT64 AcrnMemSizeAbove4gb;
   UINT32 Size;
   UINTN  CmosIndex;
+
+  if (AcrnGetSystemMemorySizeAbove4gb (&AcrnMemSizeAbove4gb) == RETURN_SUCCESS) {
+    return AcrnMemSizeAbove4gb;
+  }
 
   //
   // CMOS 0x5b-0x5d specifies the system memory above 4GB MB.

OvmfPkg/PlatformPei/Platform.h
@@ -111,6 +111,16 @@ XenPublishRamRegions (
   );
 
 RETURN_STATUS
+AcrnGetSystemMemorySizeBelow4gb (
+  OUT  UINT32               *MemSize OPTIONAL
+  );
+
+RETURN_STATUS
+AcrnGetSystemMemorySizeAbove4gb (
+  OUT  UINT64               *MemSize OPTIONAL
+  );
+
+RETURN_STATUS
 AcrnGetFirstNonAddress (
   OUT  UINT64               *MaxAddress OPTIONAL
   );

commit 408fd4cbae07ff06c6e9ad3166c2071ea2383253

acrn: fix how to get memory below 4G
Fixes: e7c3a00353 ("Use ACRN E820 map to query system memory size")

In determine the range of memory to use during PEI, it assume the memory
below 4G or above 4G is continuous. However, if there's GPU DSM region
(which needs identical map), it would break the low memory into two
parts. In this case, we could let the OVMF see the lowmem part1.

Signed-off-by: Fei Li <fei1.li@intel.com>

OvmfPkg/PlatformPei/Acrn.c
@@ -63,10 +63,14 @@ AcrnGetSystemMemorySizeBelow4gb (
     return RETURN_UNSUPPORTED;
   }
 
-  for (Loop = (INT32)(E820->E820EntriesCount - 1), Entry = &E820->E820Map[Loop];
-       Loop >= 0; Entry = &E820->E820Map[--Loop]) {
-    if (Entry->BaseAddr + Entry->Length < BASE_4GB &&
-        Entry->Type == EfiAcpiAddressRangeMemory) {
+  //
+  // ACRN e820 tables are sorted and the low memory address
+  // is start larger than 1MB
+  //
+  for (loop = 0; loop < E820->E820EntriesCount; loop++) {
+    Entry = &E820->E820Map[Loop];
+    if (Entry->Type == EfiAcpiAddressRangeMemory &&
+        Entry->BaseAddr >= 0x100000)
       *MemSize = Entry->BaseAddr + Entry->Length;
       return RETURN_SUCCESS;
     }